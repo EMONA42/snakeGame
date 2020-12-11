196a1de103621611d4b4db3c3d11458258ba1f55
EMONA42 <69512837+EMONA42@users.noreply.github.com>
2020-12-12 01:40:41 +0800

    Update Window.java

--- SnakeGame/SnakeGame/src/snakegame/Window.java
+++ SnakeGame/SnakeGame/src/snakegame/Window.java
@@ -12,7 +12,7 @@ import javax.swing.JFrame;
 
 /**
  *
- * @author LerLerChan
+ * @author EMONAF2000003A
  */
 class Window extends JFrame{
     private static final long serialVersionUID = -2542001418764869760L;
